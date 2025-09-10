a6c6f80453af2b02137a9d0b6ab2b3f2bf385457
Maksim <78586711+Mursulov@users.noreply.github.com>
2025-09-10 22:27:55 +0300

    Update FigureTests.cs

--- tests/GeometryLib.Tests/FigureTests.cs
+++ tests/GeometryLib.Tests/FigureTests.cs
@@ -21,7 +21,7 @@ namespace GeometryLib.Tests
             var b = new Point(3, 0);
             var c = new Point(0, 4);
             var fig = new Figure(a, b, c);
-            // sides: 3, 5, 4 -> perimeter 12
+           
             Assert.Equal(12.0, fig.PerimeterCalculator(), 6);
         }
 
@@ -34,16 +34,6 @@ namespace GeometryLib.Tests
             var d = new Point(0, 1);
             var fig = new Figure(a, b, c, d);
             Assert.Equal(4.0, fig.PerimeterCalculator(), 6);
-        }
-
-        [Fact]
-        public void Constructor_ValidatesPointCount()
-        {
-            var ex = Assert.Throws<ArgumentException>(() => {
-                // construct via vector-like path is private; but creating with wrong number via reflection is unnecessary
-                throw new ArgumentException();
-            });
-            Assert.NotNull(ex);
         }
     }
 }
